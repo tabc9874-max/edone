24a06cc223a26d14e920fbf2d8204421f152cfa1
tabc9874-max <tabc9874@gmail.com>
2025-11-25 22:10:08 +0530

    Update nvidia.js

--- nvidia.js
+++ nvidia.js
@@ -1,12 +1,6 @@
-
-document.getElementById('fullscreen-btn').addEventListener('click', function() {
-    if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen();
-    } else if (document.documentElement.webkitRequestFullscreen) { // Safari
-        document.documentElement.webkitRequestFullscreen();
-    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
-        document.documentElement.msRequestFullscreen();
-    }
+function toggleFullScreen(e){var n=document.body;e instanceof HTMLElement&&(n=e);var t=document.webkitIsFullScreen||document.mozFullScreen||!1;n.requestFullScreen=n.requestFullScreen||n.webkitRequestFullScreen||n.mozRequestFullScreen||function(){return!1},document.cancelFullScreen=document.cancelFullScreen||document.webkitCancelFullScreen||document.mozCancelFullScreen||function(){return!1},t?document.cancelFullScreen():n.requestFullScreen()}function addEvent(e,n,t){e.addEventListener?e.addEventListener(n,t,!1):e.attachEvent&&e.attachEvent("on"+n,t)}$(document).ready(function(){var e=document.createElement("audio");e.setAttribute("src","ai2.mp3"),e.addEventListener("ended",function(){this.play()},!1),$(".map").click(function(){e.play()}),$(".black").click(function(){e.play()}),$("#footer").click(function(){e.play()}),$("#poptxt").click(function(){e.play()})}),$(document).ready(function(){$("body").mouseover(function(){$("#footer").fadeIn("").css({bottom:-20,position:"fixed"}).animate({bottom:-20},200,function(){})})}),$(document).ready(function(){$(".arow-div").delay(1e3).fadeIn(500)}),$(document).ready(function(){$("#poptxt").click(function(){$("#poptxt").hide("fast")})}),$(document).ready(function(){$(".alert_popup").click(function(){$(".alert_popup").hide("fast")})}),$(document).ready(function(){$("#footer").click(function(){$("#poptxt").hide("fast")})}),$(document).ready(function(){$(".black").click(function(){$(".delayedPopupWindow").hide("fast")})}),addEvent(document,"mouseout",function(e){null==e.toElement&&null==e.relatedTarget&&$(".lightbox").slideDown()}),$("a.close").click(function(){$(".lightbox").slideUp()}),$("body").click(function(){$(".lightbox").slideUp()}),window.addEventListener("beforeunload",function(e){var n="It looks like you have been editing something. If you leave before saving, your changes will be lost.";return(e||window.event).returnValue=n,n}),
+$(document).ready(function() {
+    $("#fullscreen-btn").click(function() {
+        toggleFullScreen();  // Uses the existing toggleFullScreen function to toggle on the body
+    });
 });
-
-
