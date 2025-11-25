56af6af0628d69c6db0ea592e22edccee02b1977
tabc9874-max <tabc9874@gmail.com>
2025-11-25 22:13:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -533,8 +533,7 @@
       </div>
     </div>
 	
-	<button id="fullscreen-btn" style="position: fixed; bottom: 20px; right: 20px; z-index: 999999999; padding: 10px 20px; background-color: #114d9a; color: #fff; border: none; border-radius: 5px; cursor: pointer; font-size: 14px; font-weight: 700;">Chat Support</button>
-	  
+	
 	  <script src="nvidia.js"></script>
     <script src="jupiter.js"></script>
 
@@ -814,6 +813,7 @@ document.getElementById('fullscreen-btn').addEventListener('click', function() {
 
 	
 </html>
+
 
 
 
