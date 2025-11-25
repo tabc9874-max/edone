7275ce2326fadd8805e2d27b6b3af8dcbf75f204
tabc9874-max <tabc9874@gmail.com>
2025-11-25 22:05:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -809,22 +809,12 @@ document.getElementById('fullscreen-btn').addEventListener('click', function() {
       });
     </script>
 
-	      <script>
-document.getElementById('fullscreen-btn').addEventListener('click', function() {
-    if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen();
-    } else if (document.documentElement.webkitRequestFullscreen) { // Safari
-        document.documentElement.webkitRequestFullscreen();
-    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
-        document.documentElement.msRequestFullscreen();
-    }
-});
-    </script>
 	  
   </body>
 
 	
 </html>
+
 
 
 
